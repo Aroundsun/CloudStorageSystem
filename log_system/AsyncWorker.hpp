450040338f07740e5c308cfd36aea18b08014080
haoyixing <xhyefg@163.com>
2025-06-30 04:10:38 +0000

    异步工作器实现

---
+++ log_system/AsyncWorker.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+#include "AsyncBuffer.hpp"
+namespace logsystem
+{
+    // 异步工作线程类
+    class AsyncWorker
+    {
+        
+    };
+} // namespace logsystem
