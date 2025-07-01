e35f4ca376685942b2026c1d69cf676bc39eb99f
haoyixing <xhyefg@163.com>
2025-07-01 02:09:16 +0000

    完善日志刷新类

--- log_system/LogFlush.hpp
+++ log_system/LogFlush.hpp
@@ -95,11 +95,11 @@ namespace logsystem
             }
             else if (config->flush_log == 2)
             {
-                ofs_.flush();                                 // ① 用户缓冲 → 内核
-                int fd = ::open(filename_.c_str(), O_RDONLY); // ② 重新拿 fd
+                ofs_.flush();                                 // 用户缓冲 → 内核
+                int fd = ::open(filename_.c_str(), O_WRONLY); //  重新拿 fd
                 if (fd >= 0)
                 {
-                    ::fsync(fd); // ③ 内核 → 硬盘
+                    ::fsync(fd); // 内核 → 硬盘
                     ::close(fd);
                 }
             }
@@ -117,11 +117,115 @@ namespace logsystem
     // 滚动文件日志刷新类
     class RollingFileFlush : public LogFlush
     {
+    public:
+        using ptr = std::shared_ptr<RollingFileFlush>;
+        RollingFileFlush(std::string basename, size_t max_size)
+            : base_name_(basename), max_size_(max_size), current_size_(0), cnt_(0), filename_("")
+        {
+            std::string path = logsystem::File::Path(basename); // 获取日志文件所在的目录
+            if (!path.empty())
+            {
+                logsystem::File::CreateDirectory(path); // 创建目录
+            }
+        }
+        void Flush(const char *data, size_t len) override
+        {
+            // 确保日志文件大小不满足滚动要求
+            InitLogFile();                                       
+            ofs_.write(data, static_cast<std::streamsize>(len)); // 将数据写入到缓冲区 用户态
+            if (!ofs_)
+            {
+                std::cerr << __FILE__ << ":" << __LINE__ << "  write log file failed" << std::endl;
+                throw std::runtime_error("ofstream write fail");
+            }
+            current_size_ += len; // 更新当前文件大小
+            if (config->flush_log == 1)
+            {
+                ofs_.flush(); // 刷新，将用户缓冲区的数据写入到内核缓冲区
+            }
+            else if (config->flush_log == 2)
+            {
+                ofs_.flush();                                 // 刷新，将用户缓冲区的数据写入到内核缓冲区
+                int fd = ::open(filename_.c_str(), O_WRONLY); //  重新拿 fd
+                if (fd >= 0)
+                {
+                    ::fsync(fd); // 内核 → 硬盘
+                    ::close(fd);
+                }
+            }
+            if (current_size_ >= max_size_) // 写完再检查是否滚动
+                InitLogFile();
+        }
+
+    private:
+        // 初始化日志文件
+        void InitLogFile()
+        {
+            if (ofs_.is_open() || current_size_ >= max_size_)
+            {
+                if (ofs_.is_open())
+                {
+                    ofs_.close();
+                }
+                std::string new_filename = CreatLogFileName(); // 创建新的日志文件名
+                if (new_filename == "")
+                {
+                    std::cerr << __FILE__ << ":" << __LINE__ << "  create log file name failed" << std::endl;
+                    throw std::runtime_error("create log file name fail");
+                }
+                ofs_.open(new_filename, std::ios::app | std::ios::binary);
+                if (!ofs_)
+                {
+                    std::cerr << __FILE__ << ":" << __LINE__ << "  open log file failed" << std::endl;
+                    perror(NULL);
+                    throw std::runtime_error("ofstream open fail");
+                }
+                // 重新统计文件大小（已存在则续写）
+                current_size_ = std::filesystem::exists(filename_)
+                                    ? std::filesystem::file_size(filename_)
+                                    : 0;
+            }
+        }
+        // 创建日志文件名
+        std::string CreatLogFileName()
+        {
+            time_t now = logsystem::Data::GetCurrentTime();
+            struct tm t;
+            localtime_r(&now, &t); // 将当前时间转换为本地时间
+            std::string filename = base_name_;
+            filename += std::to_string(t.tm_year + 1900);
+            filename += std::to_string(t.tm_mon + 1);
+            filename += std::to_string(t.tm_mday);
+            filename += std::to_string(t.tm_hour + 1);
+            filename += std::to_string(t.tm_min + 1);
+            filename += std::to_string(t.tm_sec + 1) + '-' +
+                        std::to_string(cnt_++) + ".log";
+            filename_ = filename; // 更新当前日志文件名
+
+            return filename;
+        }
+
+    private:
+        size_t current_size_;   // 当前文件大小
+        size_t max_size_;       // 最大文件大小
+        size_t cnt_;            // 记录滚动次数
+        std::ofstream ofs_;     // 输出文件流
+        std::string base_name_; // 基础文件名
+        std::string filename_;  // 当前日志文件名
     };
 
     // 工厂类用于创建不同类型的日志刷新器
     class LogFlushFactory
     {
-    };
+    public:
+        using ptr = std::shared_ptr<LogFlushFactory>;
+        LogFlushFactory() = default;
 
+        template <typename FlushType, typename... Args>
+        static std::shared_ptr<LogFlush> CreateLog(Args &&...args)
+        {
+            return std::make_shared<FlushType>(std::forward<Args>(args)...);
+        }
+
+    };
 } // namespace logsystem
